49b465b6f8021bf34b6c100bb0c577f58bb4b301
vtn25022002 <157345220+vtn25022002@users.noreply.github.com>
2026-07-19 21:37:45 +0700

    delete

--- AIStudy/aistudyhub-frontend/src/components/common/Button.jsx
+++
@@ -1,7 +0,0 @@
-export default function Button({ children, ...props }) {
-  return (
-    <button {...props} className="button">
-      {children}
-    </button>
-  )
-}
--- AIStudy/aistudyhub-frontend/src/components/common/Card.jsx
+++
@@ -1,8 +0,0 @@
-export default function Card({ title, children }) {
-  return (
-    <article className="card">
-      <h3>{title}</h3>
-      <div>{children}</div>
-    </article>
-  )
-}
